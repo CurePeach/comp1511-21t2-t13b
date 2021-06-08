d54e85300627378cf7db6f0c87286c96d587105f
Clarissa T <53034464+CurePeach@users.noreply.github.com>
2021-06-08 16:11:20 +1000

    Clarify the purpose of certain lines of code in age.c

--- 02/age.c
+++ 02/age.c
@@ -6,10 +6,21 @@
 #define LEGAL_AGE 18
 
 int main(void) {
+    // This is asking for the user's input
     printf("Enter your age: ");
+    
+    // This is obtaining the user's input
+    // To use scanf(), we need to create the variable and 
+    // we need the & in front of the name of the variable
     int age;
     scanf("%d", &age);
-    printf("Age :: %02d\n", age);
+    
+    // This is a printf() that's just checking what the value of age is
+    // What the 02 does is that it pads with 0s and 
+    // ensures every number is a minimum of two digits.
+    // If we had %03d, it pads with 0s and ensures every number is a 
+    // minimum of three digits.
+    printf("age = %02d\n", age);
     
     if (age >= LEGAL_AGE) {
         printf("Okay you're in\n");
